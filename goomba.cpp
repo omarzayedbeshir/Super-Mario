501b2d6c78e404c575c0aee5ef4f69778cfbf810
Mohamed Khaled Ahmed Ghazy <140018004+Mohamed007k@users.noreply.github.com>
2025-04-18 01:55:06 +0200

    horizontal goomba col

--- goomba.cpp
+++ goomba.cpp
@@ -22,6 +22,11 @@ void Goomba::setPlatforms(const QList<QGraphicsItem*>& platforms) {
     platformList = platforms;
 }
 
+
+void Goomba::setPipes(const QList<pipe*>& pipes) {
+    pipeList = pipes;
+}
+
 void Goomba::applyGravity() {
     velocityY += gravity;
 
@@ -43,13 +48,48 @@ void Goomba::applyGravity() {
     }
 }
 
+void Goomba::isCollidingWithPipes()
+{
+    canMove  = true;
+    const qreal eps = 2.0;
+
+    QRectF goombaRect = this->sceneBoundingRect();
+
+    for (pipe* p : pipeList) {
+        QGraphicsPixmapItem* topPart = p->getTopPart();
+        QRectF pipeTopRect = topPart->sceneBoundingRect();
+
+        if ( goombaRect.right() + eps > pipeTopRect.left() && goombaRect.left() < pipeTopRect.left())
+        {
+            canMove = false;
+        }
+        else if ( goombaRect.left()  - eps < pipeTopRect.right() && goombaRect.right() > pipeTopRect.right())
+        {
+            canMove = false;
+        }
+    }
+}
 void Goomba::move() {
-    setPos(x() + 1 * direction, y());
-    moved++;
-    if (moved >= to_move) {
+
+    isCollidingWithPipes();
+
+    if (!canMove) {
+        // on collision: flip and reset your patrol count
         direction *= -1;
         moved = 0;
+        // --and immediately step away from the obstacle:
+        setPos(x() + 1 * direction, y());
+    } else {
+        // no obstacle: go about your normal patrol
+        setPos(x() + 1 * direction, y());
+        moved++;
+        if (moved >= to_move) {
+            direction *= -1;
+            moved = 0;
+        }
     }
+
+
 }
 
 int Goomba::getHeight() {
