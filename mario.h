b1266b8476ca9fb61b72c26d712cc18dda924c0b
omarzayedbeshir <omarzayed7777@gmail.com>
2025-04-17 12:06:16 +0300

    Add gravity

--- mario.h
+++ mario.h
@@ -3,13 +3,27 @@
 
 #include <QGraphicsRectItem>
 #include <QKeyEvent>
+#include <QGraphicsItem>
+#include <QObject>
+#include <QGraphicsView>
+#include <QList>
 
-class Mario : public QGraphicsRectItem
+class Mario : public QObject, public QGraphicsRectItem
 {
+    Q_OBJECT
 public:
     Mario(int x, int y);
+    void setPlatforms(const QList<QGraphicsItem*>& platforms);
+
 private:
     void keyPressEvent(QKeyEvent *event);
+    QList<QGraphicsItem*> platformList;
+    QTimer* gravityTimer;
+    float velocityY = 0;
+    float gravity = 0.4;
+    bool onGround = false;
+private slots:
+    void applyGravity();
 };
 
 #endif // MARIO_H
