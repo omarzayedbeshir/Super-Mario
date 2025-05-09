0b7887d0b005534076b79e32aecc65825f4eec7b
omarzayed7777@gmail.com <yilaji6830@sablecc.com>
2025-05-09 10:58:52 +0300

    Made star state more robust

--- mario.cpp
+++ mario.cpp
@@ -221,6 +221,7 @@ void Mario::updateAnimation() {
 
         currentRunFrame = (currentRunFrame + 1) % 3;
         height = 16 * scale;
+        becomeBase();
         setPixmap(QPixmap(":graphics/Mario Game Assets/Mario_Small_Run" + QString::number(currentRunFrame + 1) + "_" + horizontalDirection +".png"));
     }
 }
